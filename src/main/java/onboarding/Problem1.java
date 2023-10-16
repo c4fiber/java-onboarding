926873200d70858e9300bb9810abf2dd40ca4262
HeeHeeg <qmffndoden@naver.com>
2023-10-16 21:11:44 +0900

    숫자 number가 매개변수로 주어지는 기능.

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -20,7 +20,6 @@ import java.util.Map;
  */
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-
         int answer = Integer.MAX_VALUE;
         return answer;
     }
--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,9 +1,10 @@
 package onboarding;
 
 
+import java.util.Scanner;
+
 /**
  * ### 기능 구현 목록
- *
  * - 데이터 유효성 검사
  *   - 0, 10000이상의 수, 소수가 들어가지 않는다.
  * - 숫자 number가 매개변수로 주어지는 기능.
@@ -11,6 +12,8 @@ package onboarding;
  */
 public class Problem3 {
     public static int solution(int number) {
+        Scanner sc = new Scanner(System.in);
+        int str = sc.nextInt();
         int answer = 0;
         return answer;
     }
