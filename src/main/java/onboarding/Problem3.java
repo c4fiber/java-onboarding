b0b86c262122cd55008dd4f9bff7b5c864882fb8
HeeHeeg <qmffndoden@naver.com>
2023-10-16 20:57:38 +0900

    문제3 기능 구현 목록

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,5 +1,14 @@
 package onboarding;
 
+
+/**
+ * ### 기능 구현 목록
+ *
+ * - 데이터 유효성 검사
+ *   - 0, 10000이상의 수, 소수가 들어가지 않는다.
+ * - 숫자 number가 매개변수로 주어지는 기능.
+ * - 1부터 매개변수까지 돌며 3, 6, 9의 개수를 count하여 횟수 리턴 기능.
+ */
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
