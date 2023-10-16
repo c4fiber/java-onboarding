8f86a765f605fb999cf30f9cdf585264bbf8731d
yangyanghyunjung <hyunjungyang0922.gmail.com>
2023-10-17 08:57:24 +0900

    FindAndRemoveDuplicate 충돌 해결2

--- src/main/java/Problem2/FindAndRemoveDuplicate.java
+++ src/main/java/Problem2/FindAndRemoveDuplicate.java
@@ -1,11 +1,12 @@
 package Problem2;
 import java.util.ArrayList;
 import java.util.Arrays;
+
 public class FindAndRemoveDuplicate {
     public String FindAndRemve (String cryptogram)
     {
         String[] cryptogramArray = cryptogram.split("");
-        ArrayList<String> cryptogramList = new ArrayList<>(Arrays.asList(cryptogramArray));
+        ArrayList<String> cryptogramList = new ArrayList<String>(Arrays.asList(cryptogramArray));
         for (int i = 0; i <cryptogramList.size() - 1; i++) {
             if (cryptogramList.get(i).equals(cryptogramList.get(i + 1))) {
                 cryptogramList.remove(i);
