a22ff1cc83e8c547e6f2028d8d4b075b0435edc2
jieun <92997924+JinjinBread@users.noreply.github.com>
2023-10-16 20:18:53 +0900

    Implement 3, 6, 9 find function

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -5,4 +5,41 @@ public class Problem3 {
         int answer = 0;
         return answer;
     }
+
+    public static int countClap(int number) {
+        
+    }
+
+    public static int isInThree(int number) {
+        int count = 0;
+        while(number > 0) {
+            int check_num = number % 10;
+            if (check_num == 3)
+                count++;
+            number /= 10;
+        }
+        return count;
+    }
+
+    public static int isInSix(int number) {
+        int count = 0;
+        while(number > 0) {
+            int check_num = number % 10;
+            if (check_num == 6)
+                count++;
+            number /= 10;
+        }
+        return count;
+    }
+
+    public static int isInNine(int number) {
+        int count = 0;
+        while(number > 0) {
+            int check_num = number % 10;
+            if (check_num == 9)
+                count++;
+            number /= 10;
+        }
+        return count;
+    }
 }
