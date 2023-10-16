037d7a97650980329fc1b9369f116100496578f1
HeeHeeg <qmffndoden@naver.com>
2023-10-17 06:17:00 +0900

    1부터 매개변수까지 돌며 3, 6, 9의 개수를 count하여 횟수 리턴

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 /**
  * ### 기능 구현 목록
  * - 데이터 유효성 검사
- *   - 0, 10000이상의 수, 소수가 들어가지 않는다.
+ * - 0, 10000이상의 자연수.
  * - 숫자 number가 매개변수로 주어지는 기능.
  * - 1부터 매개변수까지 돌며 3, 6, 9의 개수를 count하여 횟수 리턴 기능.
  */
@@ -14,13 +14,14 @@ public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
 
-        for (int i = number; i <= number; i++) {
-            if (i % 10 == 3 || i % 10 == 6 || i % 10 == 9) {
-                answer += 1;
-            }else {
-
+        for (int i = 1; i <= number; i++) {
+            int temp = i;
+            while (temp > 0) {
+                if (temp % 10 == 3 || temp % 10 == 6 || temp % 10 == 9){
+                    answer ++;
+                }
+            temp /= 10;
             }
-
         }
         return answer;
 
