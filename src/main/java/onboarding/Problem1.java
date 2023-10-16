7ddf824c60e394d5f3b71ace44cf5cd3facf5929
jieun <92997924+JinjinBread@users.noreply.github.com>
2023-10-16 19:33:39 +0900

    페이지 번호 게임 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,8 +4,27 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
+        int answer = result(pobi, crong);
         return answer;
+    }
+
+    public static int result(List<Integer> pobi, List<Integer> crong) {
+        // 예외 시 -1
+        // 1. 페이지가 연달아 있지 않는 경우
+        // 2. 페이지가 1 ~ 400 사이의 수가 아닌 경우
+        // 3. 왼쪽 페이지가 홀수가 아니거나 오른쪽 페이지가 짝수가 아닌 경우
+        // 4. 리스트에 아무 값도 들어있지 않은 경우 (null)
+
+        if (!isSequentialNum(pobi) || !isSequentialNum(crong) || !isInRangeNum(pobi) || !isInRangeNum(crong)
+                || !isLOddOrREven(pobi) || !isLOddOrREven(crong) || pobi.isEmpty() || crong.isEmpty())
+            return -1;
+
+        // 포비가 이기면 1, 무승부 0, 크롱이 이기면 2
+        if (pobi_max_value(pobi) > crong_max_value(crong))
+            return 1;
+        else if (pobi_max_value(pobi) < crong_max_value(crong))
+            return 2;
+        return 0;
     }
 
     public static boolean isSequentialNum(List<Integer> person) {
