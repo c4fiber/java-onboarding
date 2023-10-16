bb582ffc3a6475689af35aff56a56936f91eb26f
HeeHeeg <qmffndoden@naver.com>
2023-10-16 18:46:18 +0900

    문제1 구현 기능 수정

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -2,6 +2,20 @@ package onboarding;
 
 import java.util.List;
 
+/**
+ * ☑️ 구현 기능 목록 작성
+ * 1. 주어진 페이지 유효성 검사하는 기능.
+ *  1-1. 왼쪽에 짝수가 올 수 없다.
+ *  1-2. 오른쪽에 홀수가 올 수 없다.
+ *  1-3. 페이지 번호가 1부터 시작되고 400페이지 까지 있다.
+ * 2. 포비가 펼친 페이지의 숫자를 비교해 가장 큰 수를 구하는 기능.
+ * 3. 크롱이 펼친 페이지의 숫자를 비교해 가장 큰 수를 구하는 기능.
+ * 4. 포비와 크롱의 수를 비교하여 return하는 기능.
+ *   4-1. 포비가 이기면1  return
+ *   4-2. 크롱이 이기면2  return
+ *   4-3. 무승부면 0  return
+ *   4-4. 예외사항은 -1로 return한다.
+ */
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
