a0a7a4637cde4acaad2a786147874c2407974cfb
HeeHeeg <qmffndoden@naver.com>
2023-10-17 11:47:21 +0900

    돈의 단위 리스트 작성

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,15 +1,13 @@
 package onboarding;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
+    private static final int[] unit = {50000, 10000, 5000, 1000, 500, 100, 10, 1};
     public static List<Integer> solution(int money) {
-        List<Integer> answer = new ArrayList<>(Arrays.asList(0, 0, 0, 0, 0, 0, 0, 0, 0));
-
-
+        List<Integer> answer = new ArrayList<>(Collections.emptyList());
 
         return answer;
     }
