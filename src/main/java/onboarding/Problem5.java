a6eefd3993664be2d41b839ab2c6091151acec27
HeeHeeg <qmffndoden@naver.com>
2023-10-17 11:34:29 +0900

    초기 값을 0으로 설정한 9자리 리스트 만들기.

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,16 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>(Arrays.asList(0, 0, 0, 0, 0, 0, 0, 0, 0));
+
+
+
         return answer;
     }
 }
