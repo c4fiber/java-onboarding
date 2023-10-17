8481415c27d18d4dadced621106da5d46d273ffc
HeeHeeg <qmffndoden@naver.com>
2023-10-17 12:40:40 +0900

    오만 원권, 만 원권, 오천 원권, 천 원권, 오백원 동전, 백원 동전, 오십원 동전, 십원 동전, 일원 동전을 가장 큰 단위부터 카운트 되는 기능

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -5,10 +5,18 @@ import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
-    private static final int[] unit = {50000, 10000, 5000, 1000, 500, 100, 10, 1};
+    private static final int[] unit = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
     public static List<Integer> solution(int money) {
         List<Integer> answer = new ArrayList<>(Collections.emptyList());
 
+        for (int i = 0; i < unit.length; i++) {
+            if (money / unit[i] != 0) {
+                answer.add(money / unit[i]);
+                money = money % unit[i];
+            } else if (money / unit[i] == 0) {
+                answer.add(0);
+            }
+        }
         return answer;
     }
 }
