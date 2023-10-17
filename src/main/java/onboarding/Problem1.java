02b926e406ac80756fafcb07a74699c369acb0cf
yangyanghyunjung <hyunjungyang0922.gmail.com>
2023-10-17 09:47:17 +0900

    yangyanghunjung branch test2

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -6,6 +6,7 @@ class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         //test
+        //teset2
         return answer;
     }
 }
