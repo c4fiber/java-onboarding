0b3fc1b49aae86cbe7935ae164ea76372e928acd
swandevson <shai4956@gmail.com>
2024-09-03 04:32:08 +0900

    feat: Problem4 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,49 @@
 package onboarding;
 
 public class Problem4 {
+    private static final int MIN_LENGTH = 1;
+    private static final int MAX_LENGTH = 1000;
+
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        validate(word);
+
+        return translateWord(word);
+    }
+
+    private static String translateWord(String word) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            sb.append(translateCharacter(c));
+        }
+
+        return sb.toString();
+    }
+
+    private static char translateCharacter(char c) {
+        if(Character.isUpperCase(c)) {
+            return (char)('Z' - c + 'A');
+        } else if(Character.isLowerCase(c)) {
+            return (char)('z' - c + 'a');
+        } else {
+            return c;
+        }
+    }
+
+    private static void validate(String word) {
+        validateInput(word);
+        validateLength(word);
+    }
+
+    private static void validateInput(String word) {
+        if (word == null || word.isEmpty()) {
+            throw new IllegalArgumentException("입력값을 입력해주세요.");
+        }
+    }
+
+    private static void validateLength(String word) {
+        if (word.length() < MIN_LENGTH || word.length() > MAX_LENGTH) {
+            throw new IllegalArgumentException(String.format("입력 길이는 %d 이상 %d이하여야합니다.", MIN_LENGTH, MAX_LENGTH));
+        }
     }
 }
