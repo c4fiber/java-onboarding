ed781bbb4f9a6f936b6bf3aefa77e670d9b7d3fe
swandevson <shai4956@gmail.com>
2024-09-03 04:19:24 +0900

    feat: Problem3 기능 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,37 @@
 package onboarding;
 
+import java.util.Arrays;
+
 public class Problem3 {
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 100_000_000;
+
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+        validate(number);
+
+        return getTotalClapCount(number);
     }
+
+    private static void validate(int number) {
+        if (number < MIN_NUMBER || number > MAX_NUMBER) {
+            throw new IllegalArgumentException("Input must be between 1 and 100,000,000");
+        }
+    }
+
+    private static int getTotalClapCount(int number) {
+        int count = 0;
+        for (int i = 1; i <= number; i++) {
+            count += getEachClapCount(i);
+        }
+
+        return count;
+    }
+
+    private static int getEachClapCount(int number) {
+        return (int)Arrays.stream(String.valueOf(number).split(""))
+            .mapToInt(Integer::parseInt)
+            .filter(i -> i == 3 || i == 6 || i == 9)
+            .count();
+    }
+
 }
